785273c4c0caf2636f65305f7c9953524228edbb
Hlukhanych <hlukhanych.stanislav@student.uzhnu.edu.ua>
2023-09-20 17:36:19 +0300

    lab1

---
+++ lab1/ex1/ex1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            List<string> key = new List<string> { "C", "D", "E", "F", "G", "A", "B" };
+            List<string> value = new List<string> { "do", "re", "mi", "fa", "sol", "la", "si" };
+
+            for (int i = 0; i < key.Count; i++)
+            {
+                ht.Add(key[i], value[i]);
+            }
+
+            foreach (string k in key)
+            {
+                Console.WriteLine($"{k}: {ht[k]}");
+            }
+
+            Console.WriteLine("reverse");
+
+            key.Reverse();
+
+            foreach (string k in key)
+            {
+                Console.WriteLine($"{k}: {ht[k]}");
+            }
+
+            Console.WriteLine(ht.Count);
+
+            ht.Clear();
+        }
+    }
+}
---
+++ lab1/ex2/ex2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+
+        public static double ArithmeticMeanPar(Queue<int> queue)
+        {
+            double a = 0;
+            int count = 0;
+            int[] arr = new int[queue.Count];
+            queue.CopyTo(arr, 0);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    a += arr[i];
+                    count++;
+                }
+            }
+            return a / count;
+        }
+
+        public static double ArithmeticMeanNePar(Queue<int> queue)
+        {
+            double a = 0;
+            int count = 0;
+            int[] arr = new int[queue.Count];
+            queue.CopyTo(arr, 0);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    a += arr[i];
+                    count++;
+                }
+            }
+            return a / count;
+        }
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+
+            Console.Write("count: ");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("elem: ");
+                queue.Enqueue(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            Console.WriteLine(ArithmeticMeanPar(queue));
+            Console.WriteLine(ArithmeticMeanNePar(queue));
+        }
+    }
+}
